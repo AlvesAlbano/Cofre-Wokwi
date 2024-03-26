ac9f11b21d2c7af0db3c3a320743e4f6e35ebf82
AlvesAlbano <148901882+AlvesAlbano@users.noreply.github.com>
2024-03-26 18:01:30 -0300

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,13 +1,50 @@
 #include <Keypad.h>
 #include <LiquidCrystal_I2C.h>
 #include <ESP32Servo.h>
-
-//BROKER MQTT============================================================================================================================
 #include <WiFi.h>
 #include <PubSubClient.h>
-const int LED01 = 15;
+
+#define I2C_ADDR    0x27
+#define LCD_COLUMNS 16
+#define LCD_LINES   2
+
+const int travaPin = 12;
+const int btnApg = 17, btnIn = 16;
+const int ledV = 18, ledR = 19;
+const int buzzerPin = 2;
+
+// char notes[] = "gabygabyxzCDxzCDabywabywzCDEzCDEbywFCDEqywFGDEqi        azbC";
+// int length = sizeof(notes);
+// int beats[] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 3, 16,};
+// int tempo = 75;
+
+const String senhaCorreta = "123456"; // Senha predefinida
+const String senhaEmergencia = "ABCD*#";
+int tentativas = 0;
+
+String senha = "";
+
+const byte ROWS = 4;
+const byte COLS = 4;
+char keys[ROWS][COLS] = {
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
+};
+
+byte colPins[COLS] = { 27, 13, 23, 4 };
+byte rowPins[ROWS] = { 33, 25, 26, 14 };
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+LiquidCrystal_I2C lcd(I2C_ADDR, LCD_COLUMNS, LCD_LINES);
+Servo trava;
+
+//BROKER MQTT============================================================================================================================
+
+const int ledA = 15;
 const char* mqtt_server = "broker.hivemq.com"; //servidor mqtt
-WiFiClient espClient;             //abstrai
+WiFiClient espClient;             //criação do objeto espClient do tipo WiFiClient
 PubSubClient client(espClient);   //abstrai
 unsigned long lastMsg = 0;        //unsigned long = inteiro de 32 bits sem sinal
 #define MSG_BUFFER_SIZE  (50)     //abstrai
@@ -21,6 +58,7 @@ void conectarBroker() {
     String clientId = "";
     clientId += String(random(0xffff), HEX);
     if (client.connect(clientId.c_str())) {
+      digitalWrite(ledA,HIGH);
       Serial.println("Conectado");              
       client.subscribe("m6abcd/led");  	      //inscrição no tópico para receber mensagens
       //client.subscribe("");
@@ -38,55 +76,17 @@ void reconectarBroker() {
 void callback(char* topic, byte* payload, unsigned int lenght) {
 
   if ((char)payload[0] == 'a') {
-    digitalWrite(LED01, LOW);
+    digitalWrite(ledA, LOW);
    
   }
 
   if ((char)payload[0] == 'A') {
-    digitalWrite(LED01, HIGH);
+    digitalWrite(ledA, HIGH);
     
   }
   
 }
 //==========================================================================================================================================
-
-#define I2C_ADDR    0x27
-#define LCD_COLUMNS 16
-#define LCD_LINES   2
-
-Servo trava; 
-const int travaPin = 12;
-
-const int btnApg = 17, btnIn = 16;
-const int ledG = 18, ledR = 19;
-
-const int buzzerPin = 2;
-char notes[] = "gabygabyxzCDxzCDabywabywzCDEzCDEbywFCDEqywFGDEqi        azbC";
-int length = sizeof(notes);
-int beats[] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 3, 16,};
-int tempo = 75;
-
-const String senhaCorreta = "123456"; // Senha predefinida
-const String senhaEmergencia = "ABCD*#";
-int tentativas = 0;
-
-LiquidCrystal_I2C lcd(I2C_ADDR, LCD_COLUMNS, LCD_LINES);
-
-String senha = "";
-
-const uint8_t ROWS = 4;
-const uint8_t COLS = 4;
-char keys[ROWS][COLS] = {
-  { '1', '2', '3', 'A' },
-  { '4', '5', '6', 'B' },
-  { '7', '8', '9', 'C' },
-  { '*', '0', '#', 'D' }
-};
-
-uint8_t colPins[COLS] = { 27, 13, 23, 4 };
-uint8_t rowPins[ROWS] = { 33, 25, 26, 14 };
-
-Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 void setup() {
   Serial.begin(115200);
@@ -98,13 +98,13 @@ void setup() {
     Serial.print(".");
   }
   Serial.println(" Conectado!");
-  pinMode(LED01, OUTPUT);
+  pinMode(ledA, OUTPUT);
   conectarBroker();
 //==========================================================================================================================================
   pinMode(btnIn, INPUT_PULLUP);
   pinMode(btnApg, INPUT_PULLUP);
   pinMode(ledR, OUTPUT);
-  pinMode(ledG, OUTPUT);
+  pinMode(ledV, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
   trava.attach(travaPin);
   trava.write(180); 
@@ -163,7 +163,7 @@ void inserir(){
 void liberar(){
   trava.attach(travaPin); // Liga o sinal enviado para o servo, "liberando-o"
   tentativas = 0;
-  digitalWrite(ledG, HIGH);
+  digitalWrite(ledV, HIGH);
   trava.write(90);
   somAcesso();
   lcd.setCursor(0, 1);
@@ -191,7 +191,7 @@ void travar(){
 
 void reset(){
   delay(2000); // Exibe a mensagem por 2 segundos
-  digitalWrite(ledG, LOW);
+  digitalWrite(ledV, LOW);
   digitalWrite(ledR, LOW);
   trava.write(180);
   lcd.setCursor(0, 1);
@@ -217,42 +217,43 @@ void bloqueado(){
   }
 }
 
-void playNote(char note, int duration) {
-  char names[] = { 'c', 'd', 'e', 'f', 'g', 'x', 'a', 'z', 'b', 'C', 'y', 'D', 'w', 'E', 'F', 'q', 'G', 'i' };
-  int tones[] = { 1898, 1690, 1500, 1420, 1265, 1194, 1126, 1063, 1001, 947, 893, 843, 795, 749, 710, 668, 630, 594 };
-  for (int i = 0; i < 18; i++) {
-    if (names[i] == note) {
-      playTone(tones[i], duration);
-    }
-  }
-}
+// void playNote(char note, int duration) {
+//   char names[] = { 'c', 'd', 'e', 'f', 'g', 'x', 'a', 'z', 'b', 'C', 'y', 'D', 'w', 'E', 'F', 'q', 'G', 'i' };
+//   int tones[] = { 1898, 1690, 1500, 1420, 1265, 1194, 1126, 1063, 1001, 947, 893, 843, 795, 749, 710, 668, 630, 594 };
+//   for (int i = 0; i < 18; i++) {
+//     if (names[i] == note) {
+//       playTone(tones[i], duration);
+//     }
+//   }
+// }
 
-void playTone(int tone, int duration) {
-  for (long i = 0; i < duration * 1000L; i += tone * 2) {
-    digitalWrite(buzzerPin, HIGH);
-    delayMicroseconds(tone);
-    digitalWrite(buzzerPin, LOW);
-    delayMicroseconds(tone);
-  }
-}
+// void playTone(int tone, int duration) {
+//   for (long i = 0; i < duration * 1000L; i += tone * 2) {
+//     digitalWrite(buzzerPin, HIGH);
+//     delayMicroseconds(tone);
+//     digitalWrite(buzzerPin, LOW);
+//     delayMicroseconds(tone);
+//   }
+// }
 
 void somAcesso(){
-  for (int i = 0; i < length; i++) {
-    if (notes[i] == ' ') {
-      delay(beats[i] * tempo);
-    } else {
-      playNote(notes[i], beats[i] * tempo);
-    }
-    delay(tempo / 2);
-  }
+  // for (int i = 0; i < length; i++) {
+  //   if (notes[i] == ' ') {
+  //     delay(beats[i] * tempo);
+  //   } else {
+  //     playNote(notes[i], beats[i] * tempo);
+  //   }
+  //   delay(tempo / 2);
+  // }
+  tone(buzzerPin, 1000, 1000);
 }
 
 void somTrava(){
-  tone(buzzerPin, 262, 999);
+  tone(buzzerPin, 262, 1000);
 }
 
 void somTravaTotal(){
-  tone(buzzerPin, 400, 999);
+  tone(buzzerPin, 400, 1000);
 }
 
 
